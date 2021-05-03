817ded5b6d4f0bd82c796694a9387d1ad239b2bb
edward79519 <edward79519@gmail.com>
2021-05-03 17:57:07 +0800

    add models

--- qcmanager/admin.py
+++ qcmanager/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from qcmanager.models import Project, ProjCategory, Company, Contact
 # Register your models here.
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('psn', 'pname', 'psponsor',)
+
+
+class ProjCategoryAdmin(admin.ModelAdmin):
+    list_display = ('csn', 'cproj', 'cname', 'csponsor',)
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('cmpsn', 'cmpname', 'cmpsponsor',)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('cntname', 'cnttel', 'cntcomp')
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjCategory, ProjCategoryAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Contact, ContactAdmin)
--- qcmanager/models.py
+++ qcmanager/models.py
@@ -1,3 +1,59 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Project(models.Model):
+    psn = models.CharField(max_length=4)
+    pname = models.CharField(max_length=20)
+    ploc_county = models.CharField(max_length=20)
+    ploc_area = models.CharField(max_length=20)
+    ploc_secnum_list = models.CharField(max_length=20, null=True, blank=True)
+    psponsor = models.ForeignKey(User, on_delete=models.PROTECT, related_name='proj_sponsor')
+    pstatrtime = models.DateField()
+    pendtime = models.DateField(null=True, blank=True)
+    paddtime = models.DateTimeField(auto_now_add=True)
+    pupdatetime = models.DateTimeField(auto_now=True)
+    peditor = models.ForeignKey(User, on_delete=models.PROTECT, related_name='proj_editor')
+
+    def __str__(self):
+        return self.psn + self.pname
+
+
+class ProjCategory(models.Model):
+    csn = models.CharField(max_length=4)
+    cproj = models.ForeignKey(Project, on_delete=models.PROTECT, related_name='cate_proj')
+    cname = models.CharField(max_length=100)
+    csponsor = models.ForeignKey(User, on_delete=models.PROTECT, related_name='cate_sponsor')
+    cstatrtime = models.DateField()
+    cendtime = models.DateField(null=True, blank=True)
+    caddtime = models.DateTimeField(auto_now_add=True)
+    cupdatetime = models.DateTimeField(auto_now=True)
+    ceditor = models.ForeignKey(User, on_delete=models.PROTECT, related_name='cate_editor')
+
+    def __str__(self):
+        return self.csn + self.cname
+
+
+class Company(models.Model):
+    cmpname = models.CharField(max_length=50)
+    cmpsn = models.CharField(max_length=8, unique=True)
+    cmpsponsor = models.CharField(max_length=20)
+    cmptel = models.CharField(max_length=20)
+    cmpfax = models.CharField(max_length=20, null=True, blank=True)
+    cmpaddr = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cmpname + self.cmpsn
+
+
+class Contact(models.Model):
+    cntname = models.CharField(max_length=20)
+    cntcomp = models.ForeignKey(Company, on_delete=models.PROTECT, related_name='cnt_comp', blank=True, null=True)
+    cnttel = models.CharField(max_length=20)
+    cntemail = models.EmailField()
+
+    def __str__(self):
+        return self.cntname + self.cntcomp.cmpname
+
+
